d444b93f32291981b5ac69a235b67f06ea47ec7a
Dan Billson <dbillson@outlook.com>
2025-03-10 19:35:42 +0000

    chore: additional setup

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,19 +1,20 @@
-import { MoveRight } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
 
 export const Hero = () => (
-  <div className="w-full">
+  <div className="w-full bg-secondary/5">
     <div className="container mx-auto">
-      <div className="flex gap-8 py-20 lg:py-40 items-center justify-center flex-col">
+      <div className="flex flex-col items-center justify-center gap-8 py-20 lg:py-40">
         <div>
           <Badge variant="outline">We&apos;re cooking!</Badge>
         </div>
-        <div className="flex gap-4 flex-col">
-          <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
+        <div className="flex flex-col gap-4">
+          <h1 className="font-regular max-w-2xl text-center text-5xl tracking-tighter md:text-7xl">
             It&apos;s time to make something new
           </h1>
-          <p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
+          <p className="max-w-2xl text-center text-lg leading-relaxed tracking-tight text-muted-foreground md:text-xl">
             Your bar is stocked but you&apos;re stuck making the same drinks.
             Stop scrolling through recipes you can&apos;t make. Tell us
             what&apos;s in your bar and we&apos;ll show you the perfect
@@ -21,7 +22,8 @@ export const Hero = () => (
           </p>
         </div>
         <Button size="lg" variant="secondary">
-          Get started <MoveRight className="w-4 h-4" />
+          <Link href="#ingredients">Get started</Link>
+          <MoveRight className="h-4 w-4" />
         </Button>
       </div>
     </div>
